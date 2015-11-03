abd72f885b2613e58c55e7a08a221ce6331bd3fb
Petri Alapiessa <palapiessa@github.com>
2015-11-03 12:38:06 +0000

    initial commit

---
+++ house-manager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="house_manager.MvcApplication" Language="C#" %>
---
+++ house-manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(house_manager.Startup))]
+namespace house_manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
